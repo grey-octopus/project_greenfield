9aa7300703a47216c931cdcecfbc93b4d88aab6f
eeeeman <eeeeman@icloud.com>
2019-12-09 15:52:39 -0500

    initial setup/connected App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./app.jsx";
 import "./styles.css";
 
-
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+
+ReactDOM.render(<App />, document.getElementById("app"));
