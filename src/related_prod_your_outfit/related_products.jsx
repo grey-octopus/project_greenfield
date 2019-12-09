44839e6b7549607f5f06f56647fba1ed2d457055
eeeeman <eeeeman@icloud.com>
2019-12-09 16:19:27 -0500

    created functional components

---
+++ src/related_prod_your_outfit/related_products.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Related_Products = () => {
+  return <div>Related Products component</div>;
+};
+
+export default Related_Products;
---
+++ src/related_prod_your_outfit/your_outfit.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Your_Outfit = () => {
+  return <div>your outfit component</div>;
+};
+
+export default Your_Outfit;
