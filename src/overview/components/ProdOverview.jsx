20d429d04f548de9a646d304fb09da8eb56b9efe
Alec Champaign <alecchampaign@protonmail.com>
2019-12-10 17:46:10 -0500

    Create ProdOverview component

---
+++ src/overview/components/ProdOverview.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProdOverview = props => {
+  return (
+    <div id="prod-overview">
+      <h3>props.category</h3>
+      <h1>props.title</h1>
+      {/* PRICE */}
+    </div>
+  )
+}
+
+export default ProdOverview
