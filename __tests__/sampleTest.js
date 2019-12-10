87eec1b3b541fd6fc034ddbc4e71a7d54ce19ec1
Alec Champaign <alecchampaign@protonmail.com>
2019-12-09 19:22:18 -0500

    Create test directory with sample test

---
+++ __tests__/sampleTest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+const testComponent = props => {
+  return (<div testProps="test"></div>)
+}
+
+describe('Jest and enzyme should be setup correctly', () => {
+  it('renders without crashing', () => {
+    shallow(<testComponent />)
+  })
+})
