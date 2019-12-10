4c87b43144aacaff3ce1744ed9ea94f0b5f29af2
Alec Champaign <alecchampaign@protonmail.com>
2019-12-10 17:42:05 -0500

    Return numOfRatings property

--- src/overview/reducers/averageRatingReducer.js
+++ src/overview/reducers/averageRatingReducer.js
@@ -1,7 +1,7 @@
 const averageRatingReducer = (state = [], action) => {
   switch (action.type) {
     case 'UPDATE_AVERAGE_RATING':
-      return {...state, averageRating: action.payload}
+      return {...state, averageRating: action.payload, numOfRatings: action.numOfRatings}
     default:
       return state;
   }
