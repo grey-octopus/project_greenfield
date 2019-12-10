8abee6644270a45b65a37fe81ba4170a2d179295
Alec Champaign <alecchampaign@protonmail.com>
2019-12-10 17:46:29 -0500

    Add reviews link

--- src/overview/components/StarRating.jsx
+++ src/overview/components/StarRating.jsx
@@ -11,6 +11,7 @@ const StarRating = props => {
         <div className="stars-outer far fa-star">
           <div className="stars-inner fas fa-star" style={innerStyle}></div>
         </div>
+        <span><a link="#ratings-reviews">Read all {props.numOfRatings} reviews</a></span>
       </div>
     )
   } else return <div>Loading...</div>
