7e47159dc17df6ad4dbcf5daac12a6979594797d
Justin Haddock <jhaddock385@gmail.com>
2019-12-10 19:38:07 -0500

    merge

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReviewBrowser from './reviews/Review_Browser.jsx';
 
-// const App = () => {
-//   return <div>Hello Worldy world!</div>;
-// };
-
 const App = () => {
   return (
     <div>
--- src/reviews/Review_Browser.jsx
+++ src/reviews/Review_Browser.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import { Component } from 'react';
 
 class ReviewBrowser extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { reviews: null };
   }
 
   componentDidMount() {
     console.log('Review Browser did mount');
+
+    axios({
+      method: 'get',
+      url: 'http://3.134.102.30/reviews/1/list',
+      responseType: 'stream'
+    })
+      .then(function(response) {
+        console.log(response);
+        this.setState({ reviews: response.data.results });
+      })
+      .then(() => {
+        this.render();
+      })
+      .catch(() => {
+        console.log('review request error');
+      });
   }
 
   render() {
