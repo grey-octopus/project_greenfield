53f8baa8daad3d41b1e43be4c984a5854b38687e
Alec Champaign <alecchampaign@protonmail.com>
2019-12-10 22:42:31 -0500

    Hide StarRating component if product has no reviews

--- src/overview/components/StarRating.jsx
+++ src/overview/components/StarRating.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
 
 const StarRating = props => {
-  useEffect(() => props.updateAverageRating(props.prodId)) // dispatch action
+  useEffect(() => {
+    // dispatch thunk
+    props.updateAverageRating(props.prodId)
+  }) 
   if (props.averageRating) {
     const innerStyle = {
       width: `${(props.averageRating/5) * 100}%`
@@ -14,7 +17,7 @@ const StarRating = props => {
         <span><a link="#ratings-reviews">Read all {props.numOfRatings} reviews</a></span>
       </div>
     )
-  } else return <div>Loading...</div>
+  } else return <div id="star-rating"></div>
 }
 
 export default StarRating
