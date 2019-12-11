1edef00b0784bdc218be451b7b4440e65e653b0e
Joey Liu <joeyliu224@gmail.com>
2019-12-11 14:50:19 -0500

    create QuestionList and Question components

---
+++ src/q_and_a/components/QuestionList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { connect} from "react-redux";
+import Question from "./Question";
+
+const QuestionList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default QuestionList
