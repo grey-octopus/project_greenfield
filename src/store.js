63a760ca4ffdc790d3441575380d1c5c543671a5
Joey Liu <joeyliu224@gmail.com>
2019-12-11 16:06:44 -0500

    add fetchQuestionListReducer to store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,18 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 // import reducers to combine
-import averageRatingReducer from './overview/reducers/averageRatingReducer.js'
-import fetchProductInfoReducer from './overview/reducers/fetchProductInfoReducer.js'
+import averageRatingReducer from './overview/reducers/averageRatingReducer.js';
+import fetchProductInfoReducer from './overview/reducers/fetchProductInfoReducer.js';
+import fetchQuestionListReducer from './q_and_a/reducers/fetchQuestionListReducer.js';
 
 
 const rootReducer = combineReducers({
   averageRatingReducer,
-  fetchProductInfoReducer
+  fetchProductInfoReducer,
+  fetchQuestionListReducer
 });
 
-const initialState = {averageRatingReducer, fetchProductInfoReducer: { prodId: 1 }}
+const initialState = {averageRatingReducer, fetchProductInfoReducer: { prodId: 1 },fetchQuestionListReducer}
 
 const store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
