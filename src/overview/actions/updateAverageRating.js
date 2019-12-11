a509a18e8a3e44f44b3767bdcaf0e5bcbd744f26
Alec Champaign <alecchampaign@protonmail.com>
2019-12-10 22:40:55 -0500

    Prevent Array.reduce() from erroring out if a product has no reviews

--- src/overview/actions/updateAverageRating.js
+++ src/overview/actions/updateAverageRating.js
@@ -5,13 +5,19 @@ const updateAverageRating = (prodId) => {
     return axios.get(`http://3.134.102.30/reviews/${prodId}/meta`)
       .then(meta => {
         const ratings = meta.data.ratings
-        const weightedTotal = Object.keys(ratings).map(weight => {
-          return Number(weight) * ratings[weight]
-        }).reduce((acc, currentVal) => acc + currentVal)
-        const numOfRatings = Object.values(ratings).reduce((acc, currentVal) => acc + currentVal)
-        const roundedToNearestQuarter = (Math.round((weightedTotal / numOfRatings) * 4) / 4).toFixed(2)
-        console.log(roundedToNearestQuarter)
-        return dispatch({ type: 'UPDATE_AVERAGE_RATING', numOfRatings, payload: roundedToNearestQuarter })
+        try {
+          const weightedTotal = Object.keys(ratings).map(weight => {
+            return Number(weight) * ratings[weight]
+          }).reduce((acc, currentVal) => acc + currentVal)
+  
+          const numOfRatings = Object.values(ratings).reduce((acc, currentVal) => acc + currentVal)
+  
+          const roundedToNearestQuarter = (Math.round((weightedTotal / numOfRatings) * 4) / 4).toFixed(2)
+  
+          return dispatch({ type: 'UPDATE_AVERAGE_RATING', numOfRatings, payload: roundedToNearestQuarter })
+        } catch {
+          return dispatch({ type: 'UPDATE_AVERAGE_RATING', numOfRatings: 0, payload: 0})
+        }
       })
   }
 }
