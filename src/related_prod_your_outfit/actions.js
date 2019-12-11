e74b625a7bf9790e1a0f465b2a0c0712a1e87487
eeeeman <eeeeman@icloud.com>
2019-12-10 20:08:00 -0500

    added basic API call to get relatedProducts info. still need to get Rating using Alec's func

--- src/related_prod_your_outfit/actions.js
+++ src/related_prod_your_outfit/actions.js
@@ -1,20 +1,67 @@
 import axios from "axios";
-
+// TODO import alec's priceAvg function
 export const GET_RELATED_PRODUCTS = "GET_RELATED_PRODUCTS";
 
-export const getRelatedProducts = () => {
-  // return axios.get('3.134.102.30/')
+export const getRelatedProducts = prodId => {
+  // come back to me!
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`http://3.134.102.30/products/${prodId}/related`)
+      .catch(err => {
+        reject(err);
+      })
+      .then(({ data }) => {
+        //   console.log("related", data);
+        let productStyleCalls = data.map(id => {
+          return axios.get(`http://3.134.102.30/products/${id}/styles`);
+        });
+        let productInfoCalls = data.map(id => {
+          return axios.get(`http://3.134.102.30/products/${id}`);
+        });
+
+        Promise.all([
+          Promise.all(productInfoCalls),
+          Promise.all(productStyleCalls)
+        ]).then(array => {
+          let relatedProducts = [];
+          array[0].forEach(({ data }) => {
+            relatedProducts.push({
+              id: data.id,
+              category: data.category,
+              name: data.name
+            });
+          });
+          array[1].forEach(({ data }, i) => {
+            let price = findPriceOfProductWithDefaultStyle(data);
+            relatedProducts[i].price = price;
+          });
+          //   TODO: add rating function and property
+          resolve({ type: GET_RELATED_PRODUCTS, products: relatedProducts });
+          // return ;
+        });
+      });
+  });
 };
 
-/*
--get related product ids (GET /products/:product_id/related)
+const findPriceOfProductWithDefaultStyle = ({ results }) => {
+  let lowestPrice = 0;
+  results.forEach(product => {
+    if (product["default?"] === 1) {
+      if (product["sale_price"] !== "0") {
+        lowestPrice = product["sale_price"];
+      } else {
+        lowestPrice = product["original_price"];
+      }
+    }
+  });
+  // console.log(lowestPrice)
+  return lowestPrice;
+};
 
-For each product id:
-1) CATEGORY,NAME => get product info, take category/name from this call 
-(GET /products/:id)
-2) PRICE => get product styles, (results.styles)figure out which has a default prop of 1, not 0. (GET products/:product_id/styles)
-	-if sale price is NOT 0, display sale price
-	-else display regular price
+getRelatedProducts(2).then(result => {
+  console.log(result);
+});
+/*
 3) RATING => get product metadata and average ratings by adding up # of x_star reviews / num of reviews (GET reviews/:product_id/meta)
 -category
 -name
