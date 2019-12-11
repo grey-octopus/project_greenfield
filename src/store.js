cfba7ee1d39b200229e2bc86b0d64205c7734e89
eeeeman <eeeeman@icloud.com>
2019-12-11 14:57:46 -0500

    sorry

--- src/store.js
+++ src/store.js
@@ -1,18 +1,26 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
 // import reducers to combine
-import averageRatingReducer from './overview/reducers/averageRatingReducer.js'
-import fetchProductInfoReducer from './overview/reducers/fetchProductInfoReducer.js'
-
+import averageRatingReducer from "./overview/reducers/averageRatingReducer.js";
+import fetchProductInfoReducer from "./overview/reducers/fetchProductInfoReducer.js";
 
 const rootReducer = combineReducers({
   averageRatingReducer,
   fetchProductInfoReducer
 });
 
-const initialState = {averageRatingReducer, fetchProductInfoReducer: { prodId: 1 }}
+const initialState = {
+  averageRatingReducer,
+  fetchProductInfoReducer: { prodId: 1 }
+};
 
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
