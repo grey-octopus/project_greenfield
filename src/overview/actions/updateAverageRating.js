c10afdc4fb74ea4954ddc9eea13e606609a797af
eeeeman <eeeeman@icloud.com>
2019-12-10 20:16:16 -0500

    getting ready to merge

--- src/overview/actions/updateAverageRating.js
+++ src/overview/actions/updateAverageRating.js
@@ -1,19 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
-const updateAverageRating = (prodId) => {
+const updateAverageRating = prodId => {
   return dispatch => {
-    return axios.get(`http://3.134.102.30/reviews/${prodId}/meta`)
+    return axios
+      .get(`http://3.134.102.30/reviews/${prodId}/meta`)
       .then(meta => {
-        const ratings = meta.data.ratings
-        const weightedTotal = Object.keys(ratings).map(weight => {
-          return Number(weight) * ratings[weight]
-        }).reduce((acc, currentVal) => acc + currentVal)
-        const numOfRatings = Object.values(ratings).reduce((acc, currentVal) => acc + currentVal)
-        const roundedToNearestQuarter = (Math.round((weightedTotal / numOfRatings) * 4) / 4).toFixed(2)
-        console.log(roundedToNearestQuarter)
-        return dispatch({ type: 'UPDATE_AVERAGE_RATING', numOfRatings, payload: roundedToNearestQuarter })
-      })
-  }
-}
+        const ratings = meta.data.ratings;
+        const weightedTotal = Object.keys(ratings)
+          .map(weight => {
+            return Number(weight) * ratings[weight];
+          })
+          .reduce((acc, currentVal) => acc + currentVal);
+        const numOfRatings = Object.values(ratings).reduce(
+          (acc, currentVal) => acc + currentVal
+        );
+        const roundedToNearestQuarter = (
+          Math.round((weightedTotal / numOfRatings) * 4) / 4
+        ).toFixed(2);
+        console.log(roundedToNearestQuarter);
+        return dispatch({
+          type: "UPDATE_AVERAGE_RATING",
+          numOfRatings,
+          payload: roundedToNearestQuarter
+        });
+      });
+  };
+};
 
-export default updateAverageRating
+export default updateAverageRating;
