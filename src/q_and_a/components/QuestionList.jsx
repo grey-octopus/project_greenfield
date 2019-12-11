36dd37d966bdfee16bbc4b74508034c69f3bc3d4
Joey Liu <joeyliu224@gmail.com>
2019-12-11 17:11:18 -0500

    implement QuestionList component

--- src/q_and_a/components/QuestionList.jsx
+++ src/q_and_a/components/QuestionList.jsx
@@ -3,12 +3,28 @@ import { connect} from "react-redux";
 import Question from "./Question";
 
 const QuestionList = (props) => {
+    if(props.questionList.length === 0) {
+        return (
+            <div>
+                <AddAQuestion />
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                {
+                    props.questionList.map(
+                        (q) => {
+                            return <Question key={q.question_id}/>
+                        }
+                    )
+                }
+                <MoreAnsweredQuestions />
+                <AddAQuestion />
+            </div>
+        )
+    }
     
-    return (
-        <div>
-            
-        </div>
-    )
 }
 
 export default QuestionList
