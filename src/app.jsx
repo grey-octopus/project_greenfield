8ac97a76eebab31fc03b7ca90c44064e226ccc03
Alec Champaign <alecchampaign@protonmail.com>
2019-12-12 16:42:54 -0500

    Render default components for testing

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import OverviewContainer from './overview/containers/OverviewContainer.jsx'
+import ProdOverviewContainer from './overview/containers/ProdOverviewContainer.jsx'
 
 const App = () => {
-  return <OverviewContainer />;
+  return (
+    <div>
+      <OverviewContainer />
+      <ProdOverviewContainer />
+    </div>
+  );
 };
 
 export default App;
