fbfb31b5b81605661eb89a53697bef134a8e6e5d
Alec Champaign <alecchampaign@protonmail.com>
2019-12-12 14:57:00 -0500

    Refactor test formatting and add test for action

--- __tests__/overview.js
+++ __tests__/overview.js
@@ -14,85 +14,103 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history'
 import { Provider } from 'react-redux'
 import toJson from 'enzyme-to-json'
+import fetchProductOverview from '../src/overview/actions/fetchProductOverview.js'
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-describe("async actions", () => {
-  it ('creates action with average rating', () => {
-    const store = mockStore({ prodId: 1 })
-    const expectedActions = [{ type: 'UPDATE_AVERAGE_RATING', numOfRatings: 7, payload: '4.00' }]
-    return store.dispatch(updateAverageRating(1)).then(() => {
-      expect(store.getActions()).toEqual(expectedActions)
+describe("actions", () => {
+  describe('updateAverageRating', () => {
+    it ('creates action with average rating', () => {
+      const store = mockStore({ prodId: 1 })
+      const expectedActions = [{ type: 'UPDATE_AVERAGE_RATING', numOfRatings: 7, payload: '4.00' }]
+      return store.dispatch(updateAverageRating(1)).then(() => {
+        expect(store.getActions()).toEqual(expectedActions)
+      })
+    })
+  
+    it ('should round payload to nearest quarter', () => {
+      const store = mockStore({ prodId: 2})
+      const expectedActions = [{ type: 'UPDATE_AVERAGE_RATING', numOfRatings: 5, payload: '3.50'}]
+      return store.dispatch(updateAverageRating(2)).then(() => {
+        expect(store.getActions()).toEqual(expectedActions)
+      })
     })
   })
 
-  it ('should round payload to nearest quarter', () => {
-    const store = mockStore({ prodId: 2})
-    const expectedActions = [{ type: 'UPDATE_AVERAGE_RATING', numOfRatings: 5, payload: '3.50'}]
-    return store.dispatch(updateAverageRating(2)).then(() => {
-      expect(store.getActions()).toEqual(expectedActions)
-    })
-  })
-
-  it("should fetch product info", () => {
+  it('fetchProductInfo', () => {
     const store = mockStore({ prodId: 1 });
     const expectedActions = [
       {
         type: "FETCH_PROD_INFO",
         title: "Camo Onesie",
-        category: "Jackets",
-        description:
-          "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
-        slogan: "Blend in to your crowd",
-        features: [
-          {
-            feature: "Buttons",
-            value: "Brass"
-          }
-        ]
+        category: "Jackets"
       }
     ];
     return store.dispatch(fetchProductInfo(1)).then(() => {
       expect(store.getActions()).toEqual(expectedActions);
     });
   });
-});
 
-describe('averageRatingReducer', () => {
-  it ('should handle updateAverageRating', () => {
-    expect(averageRatingReducer([], { type: 'UPDATE_AVERAGE_RATING', payload: '4.00', numOfRatings: 5 })).toEqual({ numOfRatings: 5, averageRating: '4.00' })
-  })
-})
-
-describe("fetchProductInfoReducer", () => {
-  it("FETCH_PROD_INFO", () => {
-    expect(
-      fetchProductInfoReducer([], {
-        type: "FETCH_PROD_INFO",
-        title: "Fancy Jeans",
-        category: "Jeans"
-      })
-    ).toEqual({
-      title: "Fancy Jeans",
-      category: "Jeans"
-    });
-  });
-
-  it('FETCH_PROD_OVERVIEW', () => {
-    expect(fetchProductInfoReducer([], {
-      type: 'FETCH_PROD_OVERVIEW',
-      category: "Jeans",
-      description: "These jeans are fancy",
-      slogan: "no",
-      features: []
-    })).toEqual({
-      description: "These jeans are fancy",
-      slogan: "no",
-      features: []
+  it('fetchProductOverview', () => {
+    const store = mockStore();
+    const expectedActions = [
+      {
+        type: 'FETCH_PROD_OVERVIEW',
+        slogan: 'Blend in to your crowd',
+        description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+        features: [
+          {
+              "feature": "Buttons",
+              "value": "Brass"
+          }
+        ]
+      }
+    ]
+    return store.dispatch(fetchProductOverview(1)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
     })
   })
 });
+
+describe('reducers', () => {
+  describe('averageRatingReducer', () => {
+    it ('should handle updateAverageRating', () => {
+      expect(averageRatingReducer([], { type: 'UPDATE_AVERAGE_RATING', payload: '4.00', numOfRatings: 5 })).toEqual({ numOfRatings: 5, averageRating: '4.00' })
+    })
+  })
+  
+  describe("fetchProductInfoReducer", () => {
+    it("FETCH_PROD_INFO", () => {
+      expect(
+        fetchProductInfoReducer([], {
+          type: "FETCH_PROD_INFO",
+          title: "Fancy Jeans",
+          category: "Jeans"
+        })
+      ).toEqual({
+        title: "Fancy Jeans",
+        category: "Jeans"
+      });
+    });
+  
+    it('FETCH_PROD_OVERVIEW', () => {
+      expect(fetchProductInfoReducer([], {
+        type: 'FETCH_PROD_OVERVIEW',
+        category: "Jeans",
+        description: "These jeans are fancy",
+        slogan: "no",
+        features: []
+      })).toEqual({
+        description: "These jeans are fancy",
+        slogan: "no",
+        features: []
+      })
+    })
+  });
+})
+
+
 
 describe("<ProdOverview />", () => {
   it("renders with required props", () => {
