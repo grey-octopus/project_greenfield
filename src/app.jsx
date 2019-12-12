028e2b2204cbfc88242b8947fb2d83373759484d
eeeeman <eeeeman@icloud.com>
2019-12-12 14:20:21 -0500

    made branch

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,11 @@ import RelatedProducts from "./related_prod_your_outfit/related_products.jsx";
 import { Link, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  const { prodId } = useParams();
-  return <div>Product ID: {prodId}</div>;
+  return (
+    <div>
+      <RelatedProducts />
+    </div>
+  );
 };
 
 export default App;
