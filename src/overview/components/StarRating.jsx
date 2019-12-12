fe42b20eb2fc4c32374f9361ab4377f4a3b027d4
Alec Champaign <alecchampaign@protonmail.com>
2019-12-11 19:49:11 -0500

    Refactor components to fetch prodId from params

--- src/overview/components/StarRating.jsx
+++ src/overview/components/StarRating.jsx
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const StarRating = props => {
-  useEffect(() => { props.updateAverageRating(props.prodId) }) // dispatch action
+  const { prodId } = useParams();
+  useEffect(() => {
+    props.updateAverageRating(prodId);
+  });
   if (props.averageRating) {
     const innerStyle = {
-      width: `${(props.averageRating/5) * 100}%`
-    }
+      width: `${(props.averageRating / 5) * 100}%`
+    };
     return (
       <div className="star-rating">
         <div className="stars-outer far fa-star">
           <div className="stars-inner fas fa-star" style={innerStyle}></div>
         </div>
-        <span><a link="#ratings-reviews">Read all {props.numOfRatings} reviews</a></span>
+        <span>
+          <a link="#ratings-reviews">Read all {props.numOfRatings} reviews</a>
+        </span>
       </div>
-    )
-  } else return <div>Loading...</div>
-}
+    );
+  } else return <div>Loading...</div>;
+};
 
-export default StarRating
+export default StarRating;
