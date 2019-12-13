92b8c50dc785bce6b11d94eb95f4c9d6f62e3436
eeeeman <eeeeman@icloud.com>
2019-12-13 14:02:08 -0500

    Added a div in app to try to help formatting

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,15 @@
-
-import React, { useEffect, useState } from 'react';
-import OverviewContainer from './overview/containers/OverviewContainer.jsx';
-import ProdOverviewContainer from './overview/containers/ProdOverviewContainer.jsx';
-import RelatedProducts from './related_prod_your_outfit/related_products.jsx';
+import React, { useEffect, useState } from "react";
+import OverviewContainer from "./overview/containers/OverviewContainer.jsx";
+import ProdOverviewContainer from "./overview/containers/ProdOverviewContainer.jsx";
+import RelatedProducts from "./related_prod_your_outfit/related_products.jsx";
 import MyOutfits from "./related_prod_your_outfit/my_outifts_container.jsx";
-import Modal from 'react-modal';
-import { useParams, Link, Route, Switch } from 'react-router-dom';
-import ReviewBrowser from './reviews/review_containers/reviewBrowserContainer.js';
-import ReviewBreakdown from './reviews/review_containers/ReviewBreakdownContainer.js';
-import axios from 'axios';
-import polyfill from 'babel-polyfill';
-import QuestionListContainer from './q_and_a/containers/QuestionListContainer.jsx';
+import Modal from "react-modal";
+import { useParams, Link, Route, Switch } from "react-router-dom";
+import ReviewBrowser from "./reviews/review_containers/reviewBrowserContainer.js";
+import ReviewBreakdown from "./reviews/review_containers/ReviewBreakdownContainer.js";
+import axios from "axios";
+import polyfill from "babel-polyfill";
+import QuestionListContainer from "./q_and_a/containers/QuestionListContainer.jsx";
 
 // React modal has to be bound to the app element:
 Modal.setAppElement("#app");
@@ -18,20 +17,24 @@ Modal.setAppElement("#app");
 const App = () => {
   const { prodId } = useParams();
   const [dataToRender, setDataToRender] = useState(<div></div>);
-  
-  useEffect(() => {
-    axios.get(`http://3.134.102.30/products/${prodId}`).then(data => {
-      setDataToRender(
-        <div>
-          <OverviewContainer />
-          <ProdOverviewContainer />
-          <RelatedProducts />
-        <MyOutfits />
-          <QuestionListContainer />
-          <div id='ratings-reviews'>
-            <ReviewBreakdown className="review-breakdown" />
-            <ReviewBrowser className="review-browser" />
 
+  useEffect(() => {
+    axios
+      .get(`http://3.134.102.30/products/${prodId}`)
+      .then(data => {
+        setDataToRender(
+          <div>
+            <OverviewContainer />
+            <ProdOverviewContainer />
+            <div>
+              <RelatedProducts />
+              <MyOutfits />
+            </div>
+            <QuestionListContainer />
+            <div id="ratings-reviews">
+              <ReviewBreakdown className="review-breakdown" />
+              <ReviewBrowser className="review-browser" />
+            </div>
           </div>
         );
       })
