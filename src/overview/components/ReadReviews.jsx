c5e2f200aeb5a003b5cc995cf605eea02ede0e96
Alec Champaign <alecchampaign@protonmail.com>
2019-12-13 19:30:11 -0500

    Extract numOfReviews into its own component

---
+++ src/overview/components/ReadReviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReadReviews = props => {
+  return (
+    <span>
+      <a href="#ratings-reviews">Read all {props.numOfReviews} reviews</a>
+    </span>
+  )
+}
+
+export default ReadReviews
