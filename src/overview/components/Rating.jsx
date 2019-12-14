ea7a3a73e9f7da8f27a692ca9ff4fd2cd6a82716
Alec Champaign <alecchampaign@protonmail.com>
2019-12-13 19:31:13 -0500

    Render rating separately from stars

---
+++ src/overview/components/Rating.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import StarRating from './StarRating.jsx'
+import ReadReviews from './ReadReviews.jsx'
+import axios from 'axios'
+
+const Rating = props => {
+  const { prodId } = useParams()
+  const [numOfReviews, setNumOfReviews] = useState(0)
+  useEffect(() => {
+    props.updateAverageRating(prodId)
+    axios.get(`http://3.134.102.30/reviews/${prodId}/list?count=10000`).then(data => {
+      console.log(data)
+      setNumOfReviews(data.data.results.length)
+    }, [])
+  })
+  if (props.averageRating && numOfReviews) {
+    return (
+      <div id='rating'>
+        <StarRating prodId={prodId} averageRating={props.averageRating} />
+        <ReadReviews numOfReviews={numOfReviews} />
+      </div>
+    )
+  } else return <div id='rating'></div>
+}
+
+export default Rating
