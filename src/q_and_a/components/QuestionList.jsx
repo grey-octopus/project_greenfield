138a2bcee3e7f015aa2e4e9e3762fb87cc895c58
Joey Liu <joeyliu224@gmail.com>
2019-12-14 12:10:35 -0500

    load 4 questions as default

--- src/q_and_a/components/QuestionList.jsx
+++ src/q_and_a/components/QuestionList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Question from "./Question";
+import Search from "./Search";
 import { useParams } from "react-router-dom";
 //import MoreAnsweredQuestions from "./MoreAnsweredQuestions"
 import AddAQuestion from "./AddAQuestion";
@@ -7,7 +8,7 @@ import AddAQuestion from "./AddAQuestion";
 const QuestionList = props => {
   //console.log("props:",props)
   const { prodId } = useParams();
-  const [count, setCount] = useState(2);
+  const [count, setCount] = useState(4);
   useEffect(() => {
     props.fetchQuestionList(prodId);
     //props.handleCountChange(props.count)
@@ -18,19 +19,24 @@ const QuestionList = props => {
     const total = props.questionList.length;
     return (
       <div>
-        {props.questionList
-          .map((q, i) => {
-            return (
-              <Question key={q.question_id} question={props.questionList[i]} />
-            );
-          })
-          .slice(0, count)}
-        {total > count ? (
-          <button onClick={() => setCount(count + 2)}>
-            MORE ANSWERED QUESTIONS
-          </button>
-        ) : null}
-        <AddAQuestion />
+        <div id="searchBar">
+          <Search/>
+        </div>
+        <div>
+          {props.questionList
+            .map((q, i) => {
+              return (
+                <Question key={q.question_id} question={props.questionList[i]} />
+              );
+            })
+            .slice(0, count)}
+          {total > count ? (
+            <button onClick={() => setCount(count + 2)}>
+              MORE ANSWERED QUESTIONS
+            </button>
+          ) : null}
+          <AddAQuestion />
+        </div>
       </div>
     );
   } else {
