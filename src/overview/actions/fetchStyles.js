282f921057a67a7b300adbe023ae65b62fc69499
Alec Champaign <alecchampaign@protonmail.com>
2019-12-13 22:58:02 -0500

    Create actions to update state with selected style and styles

---
+++ src/overview/actions/fetchStyles.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const fetchStyles = prodId => {
+  return dispatch => {
+    return axios.get(`http://3.134.102.30/products/${prodId}/styles`).then(({ data }) => {
+      return dispatch({ type: 'FETCH_STYLES', styles: data.results })
+    })
+  }
+}
+
+export default fetchStyles
