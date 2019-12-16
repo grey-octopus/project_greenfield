54a855c36167a957d8b3205f411f55ab2df23499
Alec Champaign <alecchampaign@protonmail.com>
2019-12-16 12:13:16 -0500

    Format/style ImageGallery component

--- src/overview/components/Overview.jsx
+++ src/overview/components/Overview.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import ProdInfoContainer from '../containers/ProdInfoContainer.jsx';
 import { useParams } from 'react-router-dom';
 import RatingContainer from '../containers/RatingContainer.jsx'
 import StyleSelectorContainer from '../containers/StyleSelectorContainer.jsx'
 import ProdOverviewContainer from '../containers/ProdOverviewContainer.jsx'
+import ImageGalleryContainer from '../containers/ImageGalleryContainer.jsx'
 
 const Overview = props => {
   const { prodId } = useParams();
@@ -13,6 +15,7 @@ const Overview = props => {
   if (props.title) {
     return (
       <div id='overview'>
+        <ImageGalleryContainer />
         <RatingContainer />
         <ProdInfoContainer />
         <StyleSelectorContainer />
