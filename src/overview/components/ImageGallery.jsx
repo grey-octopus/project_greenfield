4f8f751857c55b3407743e42822cc639709d9091
Alec Champaign <alecchampaign@protonmail.com>
2019-12-17 12:27:17 -0500

    Create up arrow

--- src/overview/components/ImageGallery.jsx
+++ src/overview/components/ImageGallery.jsx
@@ -3,8 +3,11 @@ import { useParams } from 'react-router-dom'
 import ImageCarouselContainer from '../containers/ImageCarouselContainer.jsx'
 
 const handleClick = (e, props) => {
-  console.log('PROPS: ', props.position)
-  return props.updatePosition(props.position, props.styles[props.selected].photos.length)
+  return props.updatePosition(props.position, props.styles[props.selected].photos.length, true)
+}
+
+const handleUpClick = (e, props) => {
+  return props.updatePosition(props.position, props.styles[props.selected].photos.length, false)
 }
 
 const ImageGallery = props => {
@@ -16,15 +19,24 @@ const ImageGallery = props => {
     const inlineStyle = {
       backgroundImage: `url(${props.styles[props.selected].photos[props.selectedImage].url})`
     }
-    const arrow = props.styles[props.selected].photos.length > 7 ? 
+    const arrow = props.styles[props.selected].photos.length > 7 && props.position !== props.styles[props.selected].photos.length - 1 ? 
     <i 
       className="fas fa-chevron-down" 
       onClick={(e) => handleClick(e, props)}>
     </i>
     :
-    <div></div>
+    null
+    const upArrow = props.position ?
+      <i 
+        id='up-arrow'
+        className='fas fa-chevron-up'
+        onClick={(e) => handleUpClick(e, props)}>
+      </i>
+      :
+      null
     return (
       <div id='image-gallery' style={inlineStyle}>
+        {upArrow}
         <ImageCarouselContainer />
         {arrow}
       </div>
