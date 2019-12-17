478c1af06dd8593fd1924c5a5be1c520052a85e6
Alec Champaign <alecchampaign@protonmail.com>
2019-12-16 22:03:27 -0500

    Render ImageCarousel in ImageGallery and allow ImageGallery to update position

--- src/overview/containers/ImageGalleryContainer.jsx
+++ src/overview/containers/ImageGalleryContainer.jsx
@@ -1,18 +1,21 @@
 import { connect } from 'react-redux'
-import ImageGallery from '../components/ImageGallery'
+import ImageGallery from '../components/ImageGallery.jsx'
 import fetchStyles from '../actions/fetchStyles.js'
+import updatePosition from '../actions/updatePosition.js'
 
 const mapStateToProps = state => {
   return {
     styles: state.styles.styles,
     selected: state.styles.selected,
-    selectedImage: state.styles.selectedImage
+    selectedImage: state.styles.selectedImage,
+    position: state.styles.position
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchStyles: prodId => dispatch(fetchStyles(prodId))
+    fetchStyles: prodId => dispatch(fetchStyles(prodId)),
+    updatePosition: (position, numOfItems) => dispatch(updatePosition(position, numOfItems))
   }
 }
 
