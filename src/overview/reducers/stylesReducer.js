0cc1ecbc9698140821af00bf370f05313f86d3c6
Alec Champaign <alecchampaign@protonmail.com>
2019-12-16 22:00:28 -0500

    Create action creators/reducers

--- src/overview/reducers/stylesReducer.js
+++ src/overview/reducers/stylesReducer.js
@@ -4,6 +4,10 @@ const stylesReducer = (state = {}, action) => {
       return {...state, selected: action.index}
     case 'FETCH_STYLES':
       return {...state, styles: action.styles}
+    case 'UPDATE_SELECTED_IMG':
+      return {...state, selectedImage: action.payload}
+    case 'UPDATE_POS':
+      return {...state, position: action.newPosition }
     default:
       return state
   }
