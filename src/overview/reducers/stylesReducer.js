2961bf417cb482e17c9bd5394efd962143e48ce3
Alec Champaign <alecchampaign@protonmail.com>
2019-12-18 15:47:49 -0500

    Save image gallery queue in store

--- src/overview/reducers/stylesReducer.js
+++ src/overview/reducers/stylesReducer.js
@@ -8,6 +8,8 @@ const stylesReducer = (state = {}, action) => {
       return {...state, selectedImage: action.payload}
     case 'UPDATE_POS':
       return {...state, position: action.newPosition }
+    case 'UPDATE_QUEUE':
+      return {...state, queue: action.queue}
     default:
       return state
   }
