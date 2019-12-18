3a77addb3a92f046d91a3d056e6544c54932baec
Alec Champaign <alecchampaign@protonmail.com>
2019-12-18 15:48:54 -0500

    Refactor ImageGallery/Carousel to use a dequeue instead of flexbox ordering

--- src/overview/components/ImageGallery.jsx
+++ src/overview/components/ImageGallery.jsx
@@ -1,32 +1,54 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import ImageCarouselContainer from '../containers/ImageCarouselContainer.jsx'
+import Dequeue from '../dequeue.js'
 
-const handleClick = (e, props) => {
-  return props.updatePosition(props.position, props.styles[props.selected].photos.length, true)
+const handleDownClick = (e, props) => {
+  const photos = props.styles[props.selected].photos
+  props.queue.push(photos[props.queue.top - 1])
+  const newQueue = JSON.parse(JSON.stringify(props.queue))
+  newQueue.push = props.queue.push
+  newQueue.inject = props.queue.inject
+  props.updateQueue(newQueue)
 }
 
 const handleUpClick = (e, props) => {
-  return props.updatePosition(props.position, props.styles[props.selected].photos.length, false)
+  const photos = props.styles[props.selected].photos
+  props.queue.inject(photos[props.queue.bottom - 1])
+  const newQueue = JSON.parse(JSON.stringify(props.queue))
+  newQueue.push = props.queue.push
+  newQueue.inject = props.queue.inject
+  props.updateQueue(newQueue)
 }
 
 const ImageGallery = props => {
   const { prodId } = useParams()
+  
   useEffect(() => {
     props.fetchStyles(prodId)
-  }, [])
-  if (props.styles && props.selected !== undefined) {
+
+  }, [prodId])
+
+  if (props.styles && props.queue === undefined) {
+    const photos = props.styles[props.selected].photos
+    props.updateQueue(new Dequeue(7, photos.slice(0, 7)))
+  }
+
+  if (props.styles && props.selected !== undefined && props.queue) {
+    const photos = props.styles[props.selected].photos
     const inlineStyle = {
       backgroundImage: `url(${props.styles[props.selected].photos[props.selectedImage].url})`
     }
-    const arrow = props.styles[props.selected].photos.length > 7 && props.position !== props.styles[props.selected].photos.length - 1 ? 
+
+    const downArrow = props.queue.queue[props.queue.top - 2].url !== photos[photos.length - 1].url ? 
     <i 
       className="fas fa-chevron-down" 
-      onClick={(e) => handleClick(e, props)}>
+      onClick={(e) => handleDownClick(e, props)}>
     </i>
     :
     null
-    const upArrow = props.position ?
+    
+    const upArrow = !props.queue.queue[0] ?
       <i 
         id='up-arrow'
         className='fas fa-chevron-up'
@@ -34,11 +56,20 @@ const ImageGallery = props => {
       </i>
       :
       null
+
+      // const rightArrow = photos.length - 1 > 1 && photos[props.selectedImage].url !== photos[photos.length - 1].url ?
+      // <i 
+      //   className="fas fa-arrow-right"
+      //   onClick={e => handleRightClick(e, props)}>
+      // </i>
+      // :
+      // null
     return (
       <div id='image-gallery' style={inlineStyle}>
         {upArrow}
+        {/* {rightArrow} */}
         <ImageCarouselContainer />
-        {arrow}
+        {downArrow}
       </div>
     )
   } else return <div id='image-gallery'></div>
