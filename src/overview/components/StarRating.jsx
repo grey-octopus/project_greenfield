f7bae2b87343ace5b93f2a1e20acba2b0cf53906
Alec Champaign <alecchampaign@protonmail.com>
2019-12-18 15:46:50 -0500

    Remove overwriting style

--- src/overview/components/StarRating.jsx
+++ src/overview/components/StarRating.jsx
@@ -6,8 +6,8 @@ const StarRating = (props) => {
       width: `${(Number(props.averageRating) / 5) * 100}%`
     };
     return (
-      <div className="stars-outer far fa-star">
-        <div className="stars-inner fas fa-star" style={innerStyle}></div>
+      <div className="stars-outer far">
+        <div className="stars-inner fas" style={innerStyle}></div>
       </div>
     );
   } else return <div id="star-rating"></div>;
