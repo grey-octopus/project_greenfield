55b9e83bafdd9d5bacf7f61a4df412abf936e614
Alec Champaign <alecchampaign@protonmail.com>
2019-12-19 15:48:57 -0500

    Render AddToCart

--- src/overview/containers/OverviewContainer.jsx
+++ src/overview/containers/OverviewContainer.jsx
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux'
 import fetchProductInfo from '../actions/fetchProductInfo.js'
 import Overview from '../components/Overview.jsx'
+import fetchStyles from '../actions/fetchStyles.js'
 
 const mapStateToProps = state => {
   return {
-    title: state.fetchProductInfoReducer.title
+    title: state.fetchProductInfoReducer.title,
+    styles: state.styles.styles
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchProductInfo: (prodId) => dispatch(fetchProductInfo(prodId))
+    fetchProductInfo: (prodId) => dispatch(fetchProductInfo(prodId)),
+    fetchStyles: (prodId) => dispatch(fetchStyles(prodId))
   }
 }
 
