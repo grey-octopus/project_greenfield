03f0d75ebd933d2829395a65a1d2ef9c0209d721
Alec Champaign <alecchampaign@protonmail.com>
2019-12-20 13:28:41 -0500

    Convert thumbnails to icons when expanded view is enabled

--- src/overview/components/ImageCarousel.jsx
+++ src/overview/components/ImageCarousel.jsx
@@ -1,24 +1,43 @@
 import React from 'react'
 
 const ImageCarousel = props => {
-  return (
-    <div id='image-carousel'>
-      {/* map image elements to preserve their orignal indicides, filter out those without images, map to divs w/
-       onClicks bound to preserved indices */}
-      {props.queue.queue.map((image, index) => ({image, index})).filter(elem => elem.image !== null).map(elem => {
+  if (props.isExpanded) {
+    return (
+      <div id='image-carousel'>
+        {props.queue.queue.map((image, index) => ({image, index})).filter(elem => elem.image !== null).map(elem => {
         return (
           <div
-            className={elem.index !== props.selectedImage ? 'gallery-img' : 'gallery-img-selected'}
+            className={elem.index !== props.selectedImage ? 'gallery-img-icon' : 'gallery-img-icon-selected' /* CHAGE CLASSES HERE */}
             onClick={() => {
               props.updateSelectedImage(elem.index)
             }}
           >
-            <img src={elem.image['thumbnail_url']} />
+            <i class="far fa-circle"></i>
           </div>
         )
       })}
-    </div>
-  )
+      </div>
+    )
+  } else {
+    return (
+      <div id='image-carousel'>
+        {/* map image elements to preserve their orignal indicides, filter out those without images, map to divs w/
+         onClicks bound to preserved indices */}
+        {props.queue.queue.map((image, index) => ({image, index})).filter(elem => elem.image !== null).map(elem => {
+          return (
+            <div
+              className={elem.index !== props.selectedImage ? 'gallery-img' : 'gallery-img-selected'}
+              onClick={() => {
+                props.updateSelectedImage(elem.index)
+              }}
+            >
+              <img src={elem.image['thumbnail_url']} />
+            </div>
+          )
+        })}
+      </div>
+    )
+  }  
 }
 
 export default ImageCarousel
