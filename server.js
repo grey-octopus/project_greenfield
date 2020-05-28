ae3f774256a0783e34ac2202169fbd801d50a46a
Alec Champaign <alecchampaign@protonmail.com>
2020-05-28 17:05:08 -0400

    Change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './dist')));
